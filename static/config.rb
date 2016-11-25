082770a75a3847c231f43b5a85a2b0cd11dd400d
willbod <willbodansky@gmail.com>
2016-11-25 15:22:52 -0500

    New styling, white on black. Artist text put in

--- static/config.rb
+++ static/config.rb
@@ -2,7 +2,7 @@
 css_dir = "css" # by Compass.app 
 sass_dir = "css/sass" # by Compass.app 
 images_dir = "img" # by Compass.app 
-output_style = :compressed # by Compass.app 
+output_style = :expanded # by Compass.app 
 relative_assets = false # by Compass.app 
 line_comments = true # by Compass.app 
 sass_options = {:debug_info=>false} # by Compass.app 
